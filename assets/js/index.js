0f39d9025994c96f3b8fac829d1b0f034414b5b2
davimoreira06 <davimoreira291273@gmail.com>
2023-04-14 16:13:33 -0300

    js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,11 @@
-let inputDay = document.getElementById("input-day");
-let inputMonth = document.getElementById("input-month");
-let inputYear = document.getElementById("input-year");
+const inputDay = document.getElementById("input-day");
+const spanDay = document.getElementsByClassName("span-days")[0];
+
+const inputMonth = document.getElementById("input-month");
+const spanMonth = document.getElementsByClassName("span-months")[0];
+
+const inputYear = document.getElementById("input-year");
+const spanYear = document.getElementsByClassName("span-years")[0];
 
 function limitLength() {
     if (this.value.length > 2) {
@@ -14,6 +19,22 @@ function limitLengthYear() {
     }
 }
 
+function receiveDay() {
+    spanDay.innerHTML = inputDay.value;
+}
+
+function receiveMonth() {
+    spanMonth.innerHTML = inputMonth.value;
+}
+
+function receiveYear() {   
+    spanYear.innerHTML = inputYear.value;
+}
+
 inputDay.addEventListener("input", limitLength);
 inputMonth.addEventListener("input", limitLength);
 inputYear.addEventListener("input", limitLengthYear);
+
+inputDay.addEventListener("input", receiveDay);
+inputMonth.addEventListener("input", receiveMonth);
+inputYear.addEventListener("input", receiveYear);
