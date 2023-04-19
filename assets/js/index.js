07a40e0f8ed5682f339f7f600dc4db868d6aeea4
davimoreira06 <davimoreira291273@gmail.com>
2023-04-18 23:57:01 -0300

    calculando-idade

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,40 +1,58 @@
-const inputDay = document.getElementById("input-day");
-const spanDay = document.getElementsByClassName("span-days")[0];
+function calculateAge() {
+    let inputDay = parseInt(document.getElementById("input-day").value);
+    let spanDay = document.getElementsByClassName("span-days")[0];
 
-const inputMonth = document.getElementById("input-month");
-const spanMonth = document.getElementsByClassName("span-months")[0];
+    let inputMonth = parseInt(document.getElementById("input-month").value);
+    let spanMonth = document.getElementsByClassName("span-months")[0];
 
-const inputYear = document.getElementById("input-year");
-const spanYear = document.getElementsByClassName("span-years")[0];
+    let inputYear = parseInt(document.getElementById("input-year").value);
+    let spanYear = document.getElementsByClassName("span-years")[0];
+
+    let currentDate = new Date();
+    let currentYear = currentDate.getFullYear();
+    let currentMonth = currentDate.getMonth() + 1;
+    let currentDay = currentDate.getDate();
+
+    var ageYears = currentYear - inputYear;
+    var ageMonths = currentMonth - inputMonth;
+    var ageDays = currentDay - inputDay;
+
+    if (ageMonths < 0 || (ageMonths === 0 && ageDays < 0)) {
+        ageYears--;
+        ageMonths = 12 + ageMonths;
+    }
+    
+    if (!isNaN(ageYears) && !isNaN(ageMonths) && !isNaN(ageDays)) {
+        spanDay.innerHTML = ageDays;
+        spanMonth.innerHTML = ageMonths;
+        spanYear.innerHTML = ageYears;
+    }
+}
+
 
 function limitLength() {
     if (this.value.length > 2) {
       this.value = this.value.slice(0, 2);
     }
+    calculateAge();
 }
 
 function limitLengthYear() {
     if (this.value.length > 4) {
         this.value = this.value.slice(0, 4);
     }
+    calculateAge();
 }
 
-function receiveDay() {
-    spanDay.innerHTML = inputDay.value;
-}
-
-function receiveMonth() {
-    spanMonth.innerHTML = inputMonth.value;
-}
-
-function receiveYear() {   
-    spanYear.innerHTML = inputYear.value;
-}
+let inputDay = document.getElementById("input-day");
+let inputMonth = document.getElementById("input-month");
+let inputYear = document.getElementById("input-year");
 
 inputDay.addEventListener("input", limitLength);
 inputMonth.addEventListener("input", limitLength);
 inputYear.addEventListener("input", limitLengthYear);
 
-inputDay.addEventListener("input", receiveDay);
-inputMonth.addEventListener("input", receiveMonth);
-inputYear.addEventListener("input", receiveYear);
+inputDay.addEventListener("input", calculateAge);
+inputMonth.addEventListener("input", calculateAge);
+inputYear.addEventListener("input", calculateAge);
+
